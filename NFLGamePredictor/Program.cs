f7ecdc47a441d9f4c963a6b7bb0b854703054943
Ryan A. Styles <Ryan.Styles@trojans.dsu.edu>
2018-11-13 11:55:12 -0600

    Initial

---
+++ NFLGamePredictor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NFLGamePredictorCode
+{
+    class Program
+    {
+        static void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("C:\\Users\\Ryan Styles\\Documents\\PythonClass\\scraping\\Logs\\log1541999589.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+                //Console.WriteLine(items[0]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            LoadJson();
+            Console.ReadLine();
+        }
+    }
+}
